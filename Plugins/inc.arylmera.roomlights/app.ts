74d0fd16858dc757cbf34d73c3957db13950d04c
Lemer Guillaume <guillaume@lemer.be>
2023-12-26 22:21:39 +0100

    first version of devices, need to find how to get all devices

--- Plugins/inc.arylmera.roomlights/app.ts
+++ Plugins/inc.arylmera.roomlights/app.ts
@@ -1,16 +1,62 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 'use strict';
 
 import Homey from 'homey';
 
-class MyApp extends Homey.App {
+class RoomLights extends Homey.App {
 
   /**
    * onInit is called when the app is initialized.
    */
   async onInit() {
-    this.log('MyApp has been initialized');
+    this.log('RoomLights has been initialized');
+
+    const setRoomLights = this.homey.flow.getActionCard('setroomlights');
+    setRoomLights.registerRunListener(async (args) => {
+      const { room, brightness, temperature } = args;
+      this.log(`${room}, ${brightness}, ${temperature}`);
+      await this.setRoomLights(room, brightness, temperature);
+    });
+  }
+
+  async setRoomLights(room: string, brightness: number, temperature: number) {
+    // recuperate zones
+    const devices = Object.values(await Homey.devices.getDevices());
+    const zones = Object.values(await Homey.zones.getZones());
+    // setup asked zone and light from selected zone
+    let selectedZoneId: string = '';
+    zones.forEach((zone: any) => {
+      if (zone.name === room) {
+        selectedZoneId = zone.id;
+      }
+    });
+    if (selectedZoneId === '') {
+      const roomNames: string[] = [];
+      zones.forEach((zone: any) => {
+        roomNames.push(zone.name);
+      });
+      throw new Error(`Please send the name of the room as the first argument : ${roomNames}`);
+    }
+
+        // recuperate devices of wanted zone
+    const zoneDevices: any = [];
+    devices.forEach((device: any) => {
+      if (device.zone == selectedZoneId && device.class == "light") {
+        zoneDevices.push(device);
+      }
+    });
+    for (const device of zoneDevices) {
+      await device.setCapabilityValue("light_temperature", temperature);
+    
+      if (brightness != 0) {
+        device.setCapabilityValue("dim", brightness);
+      } else {
+        device.setCapabilityValue("onoff", false);
+      }
+ 
+    }
   }
 
 }
 
-module.exports = MyApp;
+module.exports = RoomLights;
