65ff46158c7473af81f551394b9525611847be97
Lemer Guillaume <guillaume@lemer.be>
2023-12-30 21:03:43 +0100

    update homey api devices

--- Plugins/inc.arylmera.roomlights/app.ts
+++ Plugins/inc.arylmera.roomlights/app.ts
@@ -3,13 +3,25 @@
 
 import Homey from 'homey';
 
+const { HomeyAPI } = require('homey-api');
+
+
 class RoomLights extends Homey.App {
+
+  devices = [];
+  zones = [];
 
   /**
    * onInit is called when the app is initialized.
    */
   async onInit() {
-    this.log('RoomLights has been initialized');
+
+    let homeyApi = await HomeyAPI.createAppAPI({
+      homey: this.homey,
+    });
+
+    this.devices = Object.values(await homeyApi.devices.getDevices());
+    this.zones = Object.values(await homeyApi.zones.getZones());
 
     const setRoomLights = this.homey.flow.getActionCard('setroomlights');
     setRoomLights.registerRunListener(async (args) => {
@@ -17,44 +29,44 @@ class RoomLights extends Homey.App {
       this.log(`${room}, ${brightness}, ${temperature}`);
       await this.setRoomLights(room, brightness, temperature);
     });
+
+    this.log('RoomLights has been initialized');
   }
 
   async setRoomLights(room: string, brightness: number, temperature: number) {
-    // recuperate zones
-    const devices = Object.values(await Homey.devices.getDevices());
-    const zones = Object.values(await Homey.zones.getZones());
-    // setup asked zone and light from selected zone
+
+       // setup asked zone and light from selected zone
     let selectedZoneId: string = '';
-    zones.forEach((zone: any) => {
+    this.zones.forEach((zone: any) => {
       if (zone.name === room) {
         selectedZoneId = zone.id;
       }
     });
     if (selectedZoneId === '') {
       const roomNames: string[] = [];
-      zones.forEach((zone: any) => {
+      this.zones.forEach((zone: any) => {
         roomNames.push(zone.name);
       });
       throw new Error(`Please send the name of the room as the first argument : ${roomNames}`);
     }
 
-        // recuperate devices of wanted zone
+    // recuperate devices of wanted zone
     const zoneDevices: any = [];
-    devices.forEach((device: any) => {
-      if (device.zone == selectedZoneId && device.class == "light") {
+    this.devices.forEach((device: any) => {
+      if (device.zone == selectedZoneId && device.class == "light" && device.capabilities.includes("dim")) {
         zoneDevices.push(device);
       }
     });
     for (const device of zoneDevices) {
       await device.setCapabilityValue("light_temperature", temperature);
-    
       if (brightness != 0) {
         device.setCapabilityValue("dim", brightness);
       } else {
-        device.setCapabilityValue("onoff", false);
+        await device.setCapabilityValue("onoff", false);
       }
  
     }
+
   }
 
 }
